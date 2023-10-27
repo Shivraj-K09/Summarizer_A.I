928757fa48ba9fb32ca38c72e206d073b053c385
Shivraj Kadgond <128947744+Shivraj-K09@users.noreply.github.com>
2023-10-27 09:51:14 +0530

    Update Demo.jsx
    Changes in the textarea

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -114,9 +114,27 @@ const Demo = () => {
     }
   };
 
-  const handleCopy = (copyUrl) => {
-    setCopied(copyUrl);
-    navigator.clipboard.writeText(copyUrl);
+  // const handleCopy = (copyUrl) => {
+  //   setCopied(copyUrl);
+  //   navigator.clipboard.writeText(copyUrl);
+  //   setTimeout(() => setCopied(false), 3000);
+  // };
+
+  const handleEnterKeyPress = async (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+
+      if (inputType === "url") {
+        await handleSubmit(e);
+      } else if (inputType === "paragraph") {
+        await handleTextSubmit(e);
+      }
+    }
+  };
+
+  const handleCopy = (copyText) => {
+    setCopied(copyText);
+    navigator.clipboard.writeText(copyText);
     setTimeout(() => setCopied(false), 3000);
   };
 
@@ -129,11 +147,20 @@ const Demo = () => {
   };
 
   const toggleHistory = () => {
-    setIsHistoryOpen((prevIsOpen) => !prevIsOpen);
+    if (inputType === "url") {
+      setIsHistoryOpen((prevIsOpen) => !prevIsOpen);
+    }
   };
 
   const historyPanelClass = {
     transition: "all 0.3s",
+  };
+
+  const historyPanelStyles = {
+    visibility: isHistoryOpen ? "visible" : "hidden",
+    opacity: isHistoryOpen ? 1 : 0,
+    transform: `translateX(${isHistoryOpen ? "0" : "100%"})`,
+    transition: "visibility 0s, opacity 0.3s ease, transform 0.3s ease",
   };
 
   return (
@@ -165,6 +192,7 @@ const Demo = () => {
                 }
                 required
                 className="url_input peer"
+                onKeyDown={handleEnterKeyPress}
               />
 
               <button
@@ -186,17 +214,18 @@ const Demo = () => {
               onSubmit={handleTextSubmit}
             >
               <textarea
-                rows="18"
+                rows="15"
                 placeholder="Enter the paragraph to summarize"
                 value={paragraph}
                 onChange={handleParagraphChange}
                 required
-                className="url_input-textarea peer resize-none"
+                className="url_input-textarea resize-none"
+                onKeyDown={handleEnterKeyPress}
               />
 
               <button
                 type="submit"
-                className="submit_btn-textarea peer-focus:border-gray-700 peer-focus:text-gray-700 mt-5 bg-[#1f1f1f] text-white px-4 py-2 rounded-md"
+                className="submit_btn-textarea md:mt-10 px-10 py-2 rounded-[30px] border-black/40 box-shadow-btn border font-bold flex items-center justify-center"
               >
                 Submit
               </button>
@@ -233,22 +262,39 @@ const Demo = () => {
                     <div className="flex items-center justify-center gap-2">
                       <div
                         className="copy_btn"
-                        onClick={() => handleCopy(article.summary)}
+                        // onClick={() => handleCopy(article.summary)}
+                        onClick={() =>
+                          handleCopy(
+                            inputType === "url"
+                              ? article.summary
+                              : summarizedParagraph
+                          )
+                        }
                         title="Copy Summary"
                       >
                         <img
-                          src={copied === article.summary ? tick : copy}
+                          src={
+                            copied ===
+                            (inputType === "url"
+                              ? article.summary
+                              : summarizedParagraph)
+                              ? tick
+                              : copy
+                          }
                           alt="Copy Icon"
-                          title="Hello"
+                          title="Copy Summary"
                           className="w-[60%] h-[60%] object-contain"
                         />
                       </div>
-                      <a
-                        className="text-sm hover:underline transition-all cursor-pointer"
-                        onClick={toggleHistory}
-                      >
-                        {isHistoryOpen ? "Hide History" : "Show History"}
-                      </a>
+
+                      {inputType === "url" && (
+                        <a
+                          className="text-sm hover:underline transition-all cursor-pointer"
+                          onClick={toggleHistory}
+                        >
+                          {isHistoryOpen ? "Hide History" : "Show History"}
+                        </a>
+                      )}
                     </div>
                   </div>
 
@@ -273,13 +319,8 @@ const Demo = () => {
               /* History URL */
               isHistoryOpen && (
                 <div
-                  className={`${
-                    isHistoryOpen
-                      ? "transform translate-x-0"
-                      : "transform translate-x-full"
-                  } flex flex-col gap-1 overflow-y-auto custom-scroll mb-5 md:mb-0 md:h-[590px] md:w-[350px] bg-white/50 py-7 px-5 rounded-[40px] ${
-                    historyPanelClass.transition
-                  }`}
+                  style={historyPanelStyles}
+                  className="flex flex-col gap-1 overflow-y-auto custom-scroll mb-5 md:mb-0 md:h-[590px] md:w-[350px] bg-white/50 py-7 px-5 rounded-[40px]"
                 >
                   <h2 className="font-satoshi font-bold text-gray-600 text-xl mb-2 border-b border-black pb-4">
                     Previous<span className="blue_gradient"> Links</span>
@@ -318,7 +359,7 @@ const Demo = () => {
                           <path d="M 10 2 L 9 3 L 5 3 L 4 3 L 4 5 L 5 5 L 5 20 C 5 21.093063 5.9069372 22 7 22 L 17 22 C 18.093063 22 19 21.093063 19 20 L 19 5 L 20 5 L 20 3 L 19 3 L 18 3 L 15 3 L 14 2 L 10 2 z M 7 5 L 17 5 L 17 20 L 7 20 L 7 5 z"></path>
                         </svg>
                       </div>
-                      <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                      <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate ">
                         {item.url}
                       </p>
                     </div>
